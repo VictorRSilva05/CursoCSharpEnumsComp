6d6a17ea5a8d60b41a49b933120d26a94f4e372b
Victor R. Silva <victorr.silva005@gmail.com>
2024-01-29 17:50:48 -0300

    created class OrderItem

---
+++ ConsoleApp5/Entities/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.Entities
+{
+    internal class OrderItem
+    {
+        public int quantity { get; set; }
+        public double price { get; set; }
+
+        public static double Subtotal(int qtd, double price)
+        {
+            return qtd * price;
+        }
+    }
+}
