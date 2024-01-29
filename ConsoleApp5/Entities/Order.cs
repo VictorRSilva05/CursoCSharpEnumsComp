602cbf164e8460be03a1493dc30e9d7263b51886
Victor R. Silva <victorr.silva005@gmail.com>
2024-01-29 17:59:44 -0300

    Created Order class

---
+++ ConsoleApp5/Entities/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp5.Entities.Enums;
+
+namespace ConsoleApp5.Entities
+{
+    internal class Order
+    {
+        public DateTime moment {  get; set; }
+        public OrderStatus status { get; set; }
+    }
+}
